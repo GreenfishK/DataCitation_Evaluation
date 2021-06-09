9dd2270c3e7ccf841a89684e44b8051549e38c55
Filip Kovacevic <f.kovacevic@gmx.at>
2021-06-09 09:41:02 +0200

    Extended basis evaluation setup with evaluation steps

--- evaluation.py
+++ evaluation.py
@@ -6,10 +6,29 @@ from rdf_data_citation import query_store as qs
 import configparser
 import logging
 import timeit
+import csv
+import os
+import pandas as pd
+from rdflib import Literal, URIRef
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 logging.getLogger().setLevel(int(config.get('LOGGING', 'level')))
+
+# Setup
+write_operations = ['insert', 'update']
+dataset_types = ['small', 'big']
+versioning_modes = ['q_perf', 'mem_sav']
+query_types = ['simple_query', 'complex_query']
+evaluation_steps = ['init_versioning', 'cite_query', 're-cite_query', 'retrieve_live_data', 'retrieve_history_data']
+rounds = list(range(1, 11))
+my_index = pd.MultiIndex.from_product(iterables=[rounds, write_operations, dataset_types, versioning_modes,
+                                                 query_types, evaluation_steps],
+                                      names=['round', 'write_operation', 'dataset', 'versioning_mode', 'query_type',
+                                             'evaluation_step'])
+eval_results = pd.DataFrame(columns=['memory', 'time'],
+                            index=my_index)
+
 
 # Init versioning
 rdf_engine = rdf.TripleStoreEngine(config.get('RDFSTORE_FHIR', 'get'), config.get('RDFSTORE_FHIR', 'post'))
@@ -26,10 +45,16 @@ metadata = ct_ut.MetaData(identifier="simple_query_fhir_eval_20210602143900",
                           publication_year="2021",
                           creator="Filip Kovacevic",
                           title="Simple FHIR query")
+dir = "FHIR/sample sets/"
+for filename in os.listdir(dir):
+    if filename.startswith("dataset") and filename.endswith("_preprocessed.csv"):
+        df = pd.read_csv(dir + filename)
+        triples = df.values.tolist()
+        time = timeit.timeit(lambda: citation.cite(simple_query_fhir, metadata), number=1)
+        print("{0} loops, best of {1}: {2} sec per loop".format(1, 1, time))
+        rdf_engine.insert_triple(triples)
 
 
-time = timeit.timeit(lambda: citation.cite(simple_query_fhir, metadata), number=1)
-print("{0} loops, best of {1}: {2} sec per loop".format(1, 1, time))
 query_store = qs.QueryStore()
 query_store._remove(config.get("QUERY", 'simple_query_fhir_checksum'))
 rdf_engine.reset_all_versions()
---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+
+
+def preprocess(dir: str):
+    """
+    # Convert triple elements to n3() format and write to datasets.
+    # E.g. http://data.ontotext.com/publishing#Mention-dbaa4de4563be5f6b927c87e09f90461c09451296f4b52b1f80dcb6e941a5acd>
+    # -->
+    # <http://data.ontotext.com/publishing#Mention-dbaa4de4563be5f6b927c87e09f90461c09451296f4b52b1f80dcb6e941a5acd>"
+
+    :param dir: Diretory where the datasets are
+    :return:
+    """
+    for filename in os.listdir(dir):
+        if filename.startswith("dataset") and filename.endswith(".csv") and not filename.endswith('_preprocessed.csv'):
+            df = pd.read_csv(dir + filename, dtype={'s': str, 'p': str, 'new_label': str})
+            df.s = "<" + df.s + ">"
+            df.p = "<" + df.p + ">"
+            df.new_label = "'" + df.new_label + "'"
+            df.to_csv(dir + filename + "_preprocessed.csv", index=False)
+        if filename == 'init_version_dataset.csv' and not filename.endswith('_preprocessed.csv'):
+            df = pd.read_csv(dir + filename, dtype={'s': str, 'p': str, 'o': str})
+            df.s = "<" + df.s + ">"
+            df.p = "<" + df.p + ">"
+            df.o = "'" + df.o + "'"
+            df.to_csv(dir + filename + "_preprocessed.csv", index=False)
+
+
+dir_fhir = "FHIR/sample sets/"
+dir_wiki = "Wikipedia/sample sets/"
+preprocess(dir_fhir)
+preprocess(dir_wiki)
