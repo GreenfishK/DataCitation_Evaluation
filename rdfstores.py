296f5366ddcc782bbcf47cfcc9fedc758dfaf24a
Filip Kovacevic <f.kovacevic@gmx.at>
2021-06-29 15:35:21 +0200

    Added todos

---
+++ rdfstores.py
@@ -0,0 +1,8 @@
+# Graph DB
+# TODO Query and write statement to Citation repository in Graph DB using rdf_data_citation
+
+# Stardog
+# TODO Query and write statement to Citation repository in Stardog using rdf_data_citation
+
+# Apache Jena
+# TODO Query and write statement to Citation repository in Apache Jena using rdf_data_citation
