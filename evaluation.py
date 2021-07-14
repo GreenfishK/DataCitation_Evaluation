f4a522a09bf73fea6bcae5a25396b846a69a5b37
Filip Kovacevic <f.kovacevic@gmx.at>
2021-07-14 23:58:46 +0200

    evaluation algorithm implemented for all cases except for evaluation procedures other than "cite query"

--- evaluation.py
+++ evaluation.py
@@ -23,28 +23,21 @@ logging.getLogger().setLevel(int(config.get('LOGGING', 'level')))
 
 # Setup
 write_operations = ['insert', 'update']
-dataset_types = ['small', 'big']
+dataset_sizes = ['small', 'big']
 versioning_modes = ['q_perf', 'mem_sav']
 query_types = ['simple_query', 'complex_query']
-evaluation_steps = ['init_versioning', 'cite_query', 're-cite_query', 'retrieve_live_data', 'retrieve_history_data']
+procedures_to_evaluate = ['init_versioning', 'cite_query', 're-cite_query', 'retrieve_live_data', 'retrieve_history_data']
 rounds = list(range(1, 11))
-my_index = pd.MultiIndex.from_product(iterables=[rounds, write_operations, dataset_types, versioning_modes,
-                                                 query_types, evaluation_steps],
+my_index = pd.MultiIndex.from_product(iterables=[rounds, write_operations, dataset_sizes, versioning_modes,
+                                                 query_types, procedures_to_evaluate],
                                       names=['round', 'write_operation', 'dataset', 'versioning_mode', 'query_type',
-                                             'evaluation_step'])
-eval_results = pd.DataFrame(columns=['memory', 'time'],
+                                             'procedure_to_evaluate'])
+eval_results = pd.DataFrame(columns=['memory_in_MB', 'time_in_seconds'],
                             index=my_index)
 
 
-# Init query store and RDF store
-query_store = qs.QueryStore()
-rdf_engine = rdf.TripleStoreEngine(config.get('GRAPHDB_RDFSTORE_FHIR', 'get'),
-                                   config.get('GRAPHDB_RDFSTORE_FHIR', 'post'))
-
-
 # FHIR
-simple_query_fhir = open("FHIR/simple_query.txt", "r").read()
-complex_query_fhir = open("FHIR/complex_query.txt", "r").read()
+
 
 citation = ct.Citation(config.get('GRAPHDB_RDFSTORE_FHIR', 'get'), config.get('GRAPHDB_RDFSTORE_FHIR', 'post'))
 metadata = ct_ut.MetaData(identifier="simple_query_fhir_eval_20210602143900",
@@ -55,40 +48,74 @@ metadata = ct_ut.MetaData(identifier="simple_query_fhir_eval_20210602143900",
                           title="Simple FHIR query")
 
 
-def update_triplestore(insert_statement: str):
-    sparql_post = SPARQLWrapper(config.get('GRAPHDB_RDFSTORE_FHIR', 'post'))
+def update_triplestore(insert_statement: str, endpoint):
+    sparql_post = SPARQLWrapper(endpoint)
     sparql_post.setHTTPAuth(DIGEST)
     sparql_post.setMethod(POST)
     sparql_post.setQuery(insert_statement)
     sparql_post.query()
 
 
-# Evaluation
-print("Start evaluation with parameters: insert, small, mem_sav, simple query, cite_query")
+def eval(write_operation: str, dataset_size: str, versioning_mode: str, query_type: str, procedure_to_evaluate: str):
+    # Evaluation
+    logging.info("Start evaluation with parameters: insert, small, mem_sav, simple query, cite_query")
 
-rdf_engine.version_all_rows(versioning_mode=VersioningMode.SAVE_MEM)
-for i in range(1, 11):
-    # Perform action and measure time and memory
-    time_start = time.perf_counter()
-    citation.cite(simple_query_fhir, metadata)
-    time_elapsed = (time.perf_counter() - time_start)
-    memMb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024.0
+    # Init parameters for evaluation
+    query_store = qs.QueryStore()
+    assert query_type in ["simple", "complex"], "Query must be either simple or complex."
+    if dataset_size == "small":
+        post_endpoint = config.get('GRAPHDB_RDFSTORE_FHIR', 'post')
+        query = open("FHIR/{0}_query.txt".format(query_type), "r").read()
+        query_checksum = "{0}_query_fhir_checksum".format(query_type)
+        delete_random_data = open("FHIR/delete_random_data.txt", "r").read()
+    elif dataset_size == "big":
+        post_endpoint = config.get('GRAPHDB_RDFSTORE_WIKI', 'post')
+        query = open("Wikipedia/{0}_query.txt".format(query_type), "r").read()
+        query_checksum = "{0}_query_wiki_checksum".format(query_type)
+        delete_random_data = open("Wikipedia/delete_random_data.txt", "r").read()
+    else:
+        raise Exception("Dataset size must either be big or small.")
+    rdf_engine = rdf.TripleStoreEngine(config.get('GRAPHDB_RDFSTORE_FHIR', 'get'),
+                                       config.get('GRAPHDB_RDFSTORE_FHIR', 'post'))
 
-    # Insert new random triples with fixed dataset size (10% of initial superset)
-    insert_random_data = open("FHIR/insert_random_data.txt", "r").read()
-    update_triplestore(insert_random_data)
+    if versioning_mode == "mem_sav":
+        rdf_engine.version_all_rows(versioning_mode=VersioningMode.SAVE_MEM)
+    elif versioning_mode == "q_perf":
+        rdf_engine.version_all_rows(versioning_mode=VersioningMode.Q_PERF)
+    else:
+        raise Exception("Please set the versioning mode either to mem_sav or q_perf.")
 
-    # Save evaluation results
-    eval_results.loc[(i, 'insert', 'small', 'mem_sav', 'simple_query', 'cite_query')] = [time_elapsed, memMb]
+    for i in range(1, 11):
+        # Perform action and measure time and memory
+        time_start = time.perf_counter()
+        citation.cite(query, metadata)
+        # TODO: procedures: 'init_versioning', 're-cite_query', 'retrieve_live_data', 'retrieve_history_data'
+        print(citation.query_utils.checksum)
+        time_elapsed = (time.perf_counter() - time_start)
+        memMB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024.0
 
-    # Remove citation from query store
-    query_store._remove(config.get("QUERY", 'simple_query_fhir_checksum'))
+        # Insert or update new random triples with fixed dataset size (10% of initial superset)
+        assert write_operation in ["insert", "update"], "Write operation must be either insert or update"
+        if dataset_size == "small":
+            insert_random_data = open("FHIR/{0}_random_data.txt".format(write_operation), "r").read()
+        elif dataset_size == "big":
+            insert_random_data = open("Wikipedia/{0}_random_data.txt".format(write_operation), "r").read()
+        else:
+            raise Exception("Dataset size must either be big or small.")
+        update_triplestore(insert_random_data, post_endpoint)
 
-# Save evaluation results to csv
-eval_results.to_csv()
+        # Save evaluation results
+        eval_results.loc[(i, 'insert', 'small', 'mem_sav', 'simple_query', 'cite_query')] = [memMB, time_elapsed]
 
-# Reset experiment environment and settings
-delete_random_data = open("FHIR/delete_random_data.txt", "r").read()
-update_triplestore(delete_random_data)
-rdf_engine.reset_all_versions()
+        # Remove citation from query store
+        if procedure_to_evaluate == "cite_query":
+            query_store._remove(config.get("QUERY", query_checksum))
+
+    # Save evaluation results to csv
+    logging.info("Saving evaluation results")
+    eval_results.to_csv("evaluation_results.csv")
+
+    # Reset experiment environment and settings
+    update_triplestore(delete_random_data, post_endpoint)
+    rdf_engine.reset_all_versions()
 
